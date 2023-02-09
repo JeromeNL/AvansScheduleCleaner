dd495a6e642171c668a4ec0f4cca7552f81e8776
JeromeNL <jw.kwetters@student.avans.nl>
2023-02-09 22:40:47 +0100

    Start of chrome storage sync

--- popup.js
+++ popup.js
@@ -1,6 +1,19 @@
 
 let btn = document.getElementById('submitBtn');
+
 btn.addEventListener('click', function() {
-    alert("button clicked");
+
+    const subjects = {
+        subOne: 'SWEN4',
+        subTwo: 'WEBPHP'
+    };
+
+    chrome.storage.sync.set({"SubjectsList": subjects}, function(){
+        console.log("success");
+    });
+
+    chrome.storage.sync.get(['SubjectsList'], function(result){
+        alert("button clicked: " + Object.values(subjects)[0]);
+    })
 
 });
