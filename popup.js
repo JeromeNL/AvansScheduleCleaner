500272079e63f5e2fb18f92c77a6a8cb1a700420
JeromeNL <jw.kwetters@student.avans.nl>
2023-02-09 23:03:54 +0100

    Submit subject from popup to object

--- popup.js
+++ popup.js
@@ -8,12 +8,15 @@ btn.addEventListener('click', function() {
         subTwo: 'WEBPHP'
     };
 
+    const subjectInput = document.querySelector('#firstSubject').value;
+    subjects.subThree = subjectInput;
+
     chrome.storage.sync.set({"SubjectsList": subjects}, function(){
         console.log("success");
     });
 
     chrome.storage.sync.get(['SubjectsList'], function(result){
-        alert("button clicked: " + Object.values(subjects)[0]);
+        alert("button clicked: " + Object.values(subjects));
     })
 
 });
