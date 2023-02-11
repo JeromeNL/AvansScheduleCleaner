13da3c7dd08f7b43ce24502106e19ec354a272c0
JeromeNL <jw.kwetters@student.avans.nl>
2023-02-11 20:40:36 +0100

    Clear button to remove all filter subjects

--- popup.js
+++ popup.js
@@ -1,11 +1,21 @@
 
-let btn = document.getElementById('submitBtn');
+let addBtn = document.getElementById('submitBtn');
+let clearBtn = document.getElementById('clearBtn')
 let allSubjects = {"subjects": []};
-btn.addEventListener('click', function() {
+addBtn.addEventListener('click', function() {
     const subjectInput = document.querySelector('#firstSubject').value;
     if(checkValidInput(subjectInput)){
         addSubject(subjectInput)
     }
+});
+
+clearBtn.addEventListener('click', function() {
+    chrome.storage.sync.get(['SubjectsList']).then((result) => {
+        chrome.storage.sync.remove("SubjectsList", function () {
+            console.log("Key subjectslist removed");
+        });
+    });
+
 });
 
     function addSubject(subjectString) {
@@ -31,7 +41,6 @@ btn.addEventListener('click', function() {
     }
 
     function removeSubject(subjectString) {
-
         chrome.storage.sync.get("SubjectsList", function (result) {
         if (result.SubjectsList != undefined) {
             for(let i = 0; i < result.SubjectsList.subjects.length; i++){
