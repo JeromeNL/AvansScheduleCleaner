5ecf37979d05dde8ce5703aeae67ee4ea5d3de12
JeromeNL <jw.kwetters@student.avans.nl>
2023-02-11 19:57:35 +0100

    Function for removing subjects added

--- popup.js
+++ popup.js
@@ -30,6 +30,33 @@ btn.addEventListener('click', function() {
         });
     }
 
+    function removeSubject(subjectString) {
+
+        chrome.storage.sync.get("SubjectsList", function (result) {
+        if (result.SubjectsList != undefined) {
+            for(let i = 0; i < result.SubjectsList.subjects.length; i++){
+                allSubjects.subjects.push(result.SubjectsList.subjects[i]);
+            };
+            chrome.storage.sync.remove("SubjectsList", function () {
+                console.log("Key subjectslist removed");
+            });
+        };
+
+        for(let i = 0; i < allSubjects.subjects.length; i++){
+            if(allSubjects.subjects[i] == subjectString){
+                allSubjects.subjects.splice(i, 1);
+                break;
+            }
+        }
+            chrome.storage.sync.set({"SubjectsList": allSubjects}, function () {
+            console.log("Key subjectsList has been set")
+            chrome.storage.sync.get(['SubjectsList']).then((result) => {
+                console.log("CURRENT subjets: " + JSON.stringify(result));
+            });
+        });
+    });
+}
+
     function checkValidInput(newInput){
         if(newInput == ""){
             return false
