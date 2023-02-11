44713b1b6841b2af7fff39abc23b710e9448e93b
JeromeNL <jw.kwetters@student.avans.nl>
2023-02-11 17:18:16 +0100

    Cleanup & refactoring

--- content.js
+++ content.js
@@ -3,45 +3,40 @@ let previousWeekButton = document.querySelector('[title="Vorige week"]');
 let nextWeekButton = document.querySelector('[title="Volgende week"]');
 let otherButtons = document.querySelectorAll('.ui-button-text');
 
-function loadFullProcess() {
-    //loadAllClassesForWeek();
-    addSubjectsToFilter();
-
-
-}
 
 window.addEventListener("load", () => {
-    loadFullProcess();
+    startFilteringSubjects();
 });
 
 previousWeekButton.addEventListener ('click', function(event) {
-    loadFullProcess();
+    startFilteringSubjects();
 });
 
 nextWeekButton.addEventListener ('click', function(event) {
-    loadFullProcess();
+    startFilteringSubjects();
 });
 
 for(let i = 0; i < otherButtons.length; i++){
     otherButtons[i].addEventListener ('click', function(event) {
-        loadFullProcess();
+        startFilteringSubjects();
     });
 }
 
-function addSubjectsToFilter(){
+function startFilteringSubjects(){
     setTimeout(function(){
         chrome.storage.sync.get(['SubjectsList']).then((result) => {
             console.log("BINNENKOMEND RESULT: " + JSON.stringify(result[Object.keys(result)[0]]))
 
             if(typeof result[Object.keys(result)[0]] == "undefined"){
-                console.log("DIT BESTATA HELEMAAL NIET!!")
+                console.log("DIT BESTAAT HELEMAAL NIET!!")
                 return;
             }
-            let waardes = Object.values(result[Object.keys(result)])
-            console.log("Waarde: " + JSON.stringify(waardes));
 
-            for(let i = 0; i < waardes.length; i++){
-                subjectsToFilter.push(waardes[i]);
+            let subjects = Object.values(result[Object.keys(result)])
+            console.log("Waarde: " + JSON.stringify(subjects));
+
+            for(let i = 0; i < subjects.length; i++){
+                subjectsToFilter.push(subjects[i]);
             }
 
             console.log("CURRENT set in subjectsToFilter: " + JSON.stringify(subjectsToFilter));
@@ -54,14 +49,6 @@ function addSubjectsToFilter(){
 function loadAllClassesForWeek() {
     setTimeout(function() {
         console.log("Chrome Extensie wordt herkend!");
-
-        // see addSubjecgtsToFilter method
-        // chrome.storage.sync.get(['SubjectsList']).then((result) => {
-        //     subjectsToFilter.push(result.SubjectsList);
-        //     console.log("CURRENT set in subjectsToFilter: " + JSON.stringify(subjectsToFilter));
-        // });
-
-        
         let allClassesForWeek = document.getElementsByClassName('wc-cal-event ui-corner-all');
         console.log(allClassesForWeek);
         removeSubjectFromTimetable(allClassesForWeek);
@@ -70,20 +57,17 @@ function loadAllClassesForWeek() {
 
 }
 
-// probleem: subjectsToFilter.subjects.length werkt niet.
+
 function removeSubjectFromTimetable(allClasses){
     console.log("CURRENT set in REMOVE: " + JSON.stringify(subjectsToFilter));
    let lengte = subjectsToFilter[0].length;
    console.log("LENGTE: " + lengte)
-    for(let index = 0; index < lengte; index++){
-        console.log("index: " + index);
-        for(let i = 0; i < allClasses.length; i++){
-            //console.log("i: " + i)
-            //console.log("content: " + subjectsToFilter[0][index]);
-            //console.log("Allclasses.innertext: " + allClasses[i].innerText)
-            if(allClasses[i].innerText.includes(subjectsToFilter[0][index])){
-                allClasses[i].remove();
-                console.log("Item REMOVED: " + allClasses[i])
+    for(let subject = 0; subject < lengte; subject++){
+        console.log("index: " + subject);
+        for(let scheduleClass = 0; scheduleClass < allClasses.length; scheduleClass++){
+            if(allClasses[scheduleClass].innerText.includes(subjectsToFilter[0][subject].toUpperCase())){
+                allClasses[scheduleClass].remove();
+                console.log("Item REMOVED: " + allClasses[scheduleClass])
             }
         }
     }
--- popup.js
+++ popup.js
@@ -1,25 +1,10 @@
 
 let btn = document.getElementById('submitBtn');
-let keyName = "SubjectsList";
-let allSubjects = {"subjects": ['MBDI']};
+let allSubjects = {"subjects": []};
 btn.addEventListener('click', function() {
     const subjectInput = document.querySelector('#firstSubject').value;
     addSubject(subjectInput)
 });
-
-    function getSubjects() {
-        let returnValue;
-        chrome.storage.sync.get(['SubjectsList']).then((result) => {
-            returnValue = result;
-        });
-        return returnValue;
-    }
-
-    function setSubjects() {
-        chrome.storage.sync.set({"SubjectsList": allSubjects}, function () {
-
-        });
-    }
 
     function addSubject(subjectString) {
         let storage = chrome.storage.sync;
@@ -28,66 +13,17 @@ btn.addEventListener('click', function() {
             if (result.SubjectsList != undefined) {
                 for(let i = 0; i < result.SubjectsList.subjects.length; i++){
                     allSubjects.subjects.push(result.SubjectsList.subjects[i]);
-                }
+                };
                 storage.remove("SubjectsList", function () {
                     console.log("Key subjectslist removed");
                 });
-            }
+            };
                 allSubjects.subjects.push(subjectString)
                 storage.set({"SubjectsList": allSubjects}, function () {
                     console.log("Key subjectsList has been set")
                     chrome.storage.sync.get(['SubjectsList']).then((result) => {
                         console.log("CURRENT subjets: " + JSON.stringify(result));
                     });
-
                 });
-
-        })
+        });
     }
-
-
-
-
-//
-//
-//     chrome.storage.sync.get(['SubjectsList'], function(result){
-//         console.log("Result of current list: " + JSON.stringify(result))
-//         if (typeof result.SubjectsList === 'undefined') {
-//             console.log("bestaat nog niet!!!!")
-//         } else {
-//             console.log("bestaat al!!!!")
-//             //let newArray = allSubjects.subjects.concat(result.SubjectsList.subjects)
-//             for(let i = 0; i < result.SubjectsList.subjects.length; i++){
-//                 console.log("lets add an item" + result.SubjectsList.subjects[i])
-//                 allSubjects.subjects.push(result.SubjectsList.subjects[i]);
-//                 console.log("De ARRAY is nu: " + JSON.stringify(allSubjects))
-//             }
-//             //allSubjects.subjects.push(newArray);
-//             console.log(JSON.stringify("bestaande items: " + result.SubjectsList.subjects));
-//             chrome.storage.sync.remove('SubjectsList');
-//         }
-//
-//
-//
-//
-//     });
-//
-//
-//     const subjectInput = document.querySelector('#firstSubject').value;
-//     console.log("Current items before add new: " + JSON.stringify(allSubjects))
-//     allSubjects.subjects.push(subjectInput.toString());
-//     console.log("Input value: " + subjectInput);
-//     console.log("new state: " + JSON.stringify(allSubjects.subjects));
-//
-//
-//     chrome.storage.sync.set({"SubjectsList": allSubjects}, function () {
-//         console.log("list with new items incl. added!")
-//     });
-//
-//
-//     chrome.storage.sync.get(('SubjectsList'), function(result){
-//         //alert("button clicked: " + result.SubjectsList);
-//         console.log("Result of current list: " + JSON.stringify(result))
-//     })
-//
-// });
