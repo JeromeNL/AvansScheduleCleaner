bda6d06ddbaf544bfe1450f4554b7690d1cde2be
JeromeNL <jw.kwetters@student.avans.nl>
2023-02-11 01:01:54 +0100

    Subject insert to content.js

--- content.js
+++ content.js
@@ -1,10 +1,12 @@
-let subjectsToFilter = ['SWEN4', 'WEBJS'];
+let subjectsToFilter = [];
 let previousWeekButton = document.querySelector('[title="Vorige week"]');
 let nextWeekButton = document.querySelector('[title="Volgende week"]');
 let otherButtons = document.querySelectorAll('.ui-button-text');
 
 function loadFullProcess() {
     loadAllClassesForWeek();
+
+
 }
 
 window.addEventListener("load", () => {
@@ -25,11 +27,22 @@ for(let i = 0; i < otherButtons.length; i++){
     });
 }
 
+function addSubjectsToFilter(){
+    chrome.storage.sync.get(['SubjectsList']).then((result) => {
+        console.log("CURRENT subjets: " + JSON.stringify(result));
+    });
+}
 
 function loadAllClassesForWeek() {
     setTimeout(function() {
         console.log("Chrome Extensie wordt herkend!");
 
+        chrome.storage.sync.get(['SubjectsList']).then((result) => {
+            subjectsToFilter.push(result.SubjectsList);
+            console.log("CURRENT set in subjectsToFilter: " + JSON.stringify(subjectsToFilter));
+        });
+
+        
         let allClassesForWeek = document.getElementsByClassName('wc-cal-event ui-corner-all');
         console.log(allClassesForWeek);
         removeSubjectFromTimetable(allClassesForWeek);
@@ -39,6 +52,7 @@ function loadAllClassesForWeek() {
 
 
 function removeSubjectFromTimetable(allClasses){
+
     for(let index = 0; index < subjectsToFilter.length; index++){
         for(let i = 0; i < allClasses.length; i++){
             if(allClasses[i].innerText.includes(subjectsToFilter[index])){
